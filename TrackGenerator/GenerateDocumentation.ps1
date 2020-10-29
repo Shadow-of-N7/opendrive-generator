08ceb55488405cd170aee013fed3d070a7e8a86f
Daniel Böck <boeck_daniel@web.de>
2020-10-29 21:17:10 +0100

    Made documentation script linux compatible (hopefully)

--- TrackGenerator/GenerateDocumentation.ps1
+++ TrackGenerator/GenerateDocumentation.ps1
@@ -1,17 +1,41 @@
 $files = Get-ChildItem $PSScriptRoot -Filter "*.py"
-
-foreach($file in $files){
-    Write-Host "$PSScriptRoot\$file"
-    pydoc.exe -w `"$PSScriptRoot\$file`"
+$os = [System.Environment]::OSVersion
+if($os -match "Unix"){
+    $linux = $true
+}
+else{
+    $linux = $false
 }
 
-$docPath = "$PSScriptRoot\GeneratedDocumentation"
+if($linux){
+    foreach($file in $files){
+        pydoc3 -w `"$PSScriptRoot\$file`"
+    }
+    $docPath = "$PSScriptRoot/GeneratedDocumentation"
+}
+else{
+    foreach($file in $files){
+        pydoc.exe -w `"$PSScriptRoot\$file`"
+    }
+    $docPath = "$PSScriptRoot\GeneratedDocumentation"
+}
+
+
 if (Test-Path $docPath){
     Remove-Item $docPath -Force -Recurse
 }
 New-Item $docPath -ItemType Directory
 
 $htmlFiles = Get-ChildItem $PSScriptRoot -Filter "*.html"
-foreach($file in $htmlFiles){
-    Move-Item "$PSScriptRoot\$file" $docPath\$file
+
+if($linux){
+    foreach($file in $htmlFiles){
+        Move-Item "$PSScriptRoot/$file" "$docPath/$file"
+    }
 }
+else{
+    foreach($file in $htmlFiles){
+        Move-Item "$PSScriptRoot\$file" "$docPath\$file"
+    }
+}
+
