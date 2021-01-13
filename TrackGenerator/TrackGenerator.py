70482e1030c2384af5dd22804e1e442793cbc767
Leo <leonard.zauner@googlemail.com>
2021-01-13 16:59:27 +0100

    Branch aufgeräumt

--- TrackGenerator/TrackGenerator.py
+++ TrackGenerator/TrackGenerator.py
@@ -24,16 +24,9 @@ def GenerateTrack(segmentCount: int, minDist: float, maxDist: float, controlPoin
     """
     # List of control points defining the corner points of the track
     controlPoints = CreateControlPoints(segmentCount, minDist, maxDist, controlPointAbberation)
-    tp = GetTrackParts2(controlPoints)
+    tp = GetTrackParts(controlPoints)
     ExportTrack2(tp)
-    PlotTrack3(tp)
-    # tp = GetTrackParts(controlPoints)
-    # PlotTrack2(tp)
-    # print(str(VectorLerp(np.array([0, 0, 0]), np.array([20, 25, 20]), 0.5)))
-    # ExportTrack2(tp)
-
-    # ExportTrack(controlPoints)
-    # PlotTrack(controlPoints)
+    PlotTrack2(tp)
 
 
 def CreateControlPoints(segmentCount: int, minDist: float, maxDist: float, controlPointAbberation: float):
@@ -64,170 +57,23 @@ def CreateControlPoints(segmentCount: int, minDist: float, maxDist: float, contr
 def GetTrackParts(controlPoints):
     tp = []
     for i in range(len(controlPoints)):
-        aStartIndex = i
-        aEndIndex = (i + 1) % len(controlPoints)
-        bStartIndex = (i + 2) % len(controlPoints)
-        bEndIndex = (i + 3) % len(controlPoints)
+        nextindex = (i + 1) % len(controlPoints)
+        endturnindex = (i + 2) % len(controlPoints)
+        start = np.array(controlPoints[i])
+        end = np.array(controlPoints[nextindex])
 
-        straightAStart = np.array(controlPoints[aStartIndex])
-        straightAEnd = np.array(controlPoints[aEndIndex])
+        straightStart = VectorLerp(start, end, 0.1)
+        straightEnd = VectorLerp(end, start, 0.1)
 
-        straightBStart = np.array(controlPoints[bStartIndex])
-        straightBEnd = np.array(controlPoints[bEndIndex])
+        tp.append(TrackParts.Straight(straightStart, straightEnd))
 
-        # straightAStart = np.array([10, 0, 1])
-        # straightAEnd = np.array([13, 0, 4])
-        # straightBStart = np.array([13, 0, 4])
-        # straightBEnd = np.array([8, 0, 9])
-
-        lenA = np.linalg.norm(straightAEnd - straightAStart)
-        lenB = np.linalg.norm(straightBEnd - straightBStart)
-
-        radius = lenA * 0.4
-        if lenB < lenA:
-            radius = lenB * 0.4
-
-        anglePhi = MathUtils.AngleBetween(straightAEnd - straightAStart, straightBEnd, straightBStart)
-        agleArcInclusive = 2 * math.asin(1 / math.tan(anglePhi / 2))
-        s = radius * (1 / math.tan(anglePhi / 2))
-
-        S = MathUtils.VectorLerpAbs(straightBStart, straightBEnd, s)
-        # K =
-
-        K1 = MathUtils.VectorLerpAbs(straightAEnd, straightAStart, k)
-        K2 = MathUtils.VectorLerpAbs(straightBStart, straightBEnd, k)
-
-        # Jetzt kommt das Arc
-        M = MathUtils.GetArcMiddle(np.array([straightAStart[0], straightAStart[2]]),
-                                   np.array([K1[0], K1[2]]),
-                                   np.array([K2[0], K2[2]]),
-                                   np.array([straightBEnd[0], straightBEnd[2]]))
-        M = np.array([M[0], 0, M[1]])
-
-        radius1 = np.linalg.norm(M - K1)
-        radius2 = np.linalg.norm(M - K2)
-        print("\n\nRadius1: " + str(radius1))
-        print("Radius2: " + str(radius2))
-
-        # aStartIndex = i
-        # aEndIndex = (i + 1) % len(controlPoints)
-        # bStartIndex = (i + 2) % len(controlPoints)
-        # bEndIndex = (i + 3) % len(controlPoints)
-        #
-        # start = np.array(controlPoints[aStartIndex])
-        # end = np.array(controlPoints[aEndIndex])
-        # straightAStart = VectorLerp(start, end, 0.1)
-        # straightAEnd = VectorLerp(end, start, 0.1)
-        #
-        # start = np.array(controlPoints[bStartIndex])
-        # end = np.array(controlPoints[bEndIndex])
-        # straightBStart = VectorLerp(start, end, 0.1)
-        # straightBEnd = VectorLerp(end, start, 0.1)
-        #
-        # tp.append(TrackParts.Straight(straightAStart, straightAEnd))
-        #
-        # # Jetzt kommt das Arc
-        # M = MathUtils.GetArcMiddle(np.array([straightAStart[0], straightAStart[2]]),
-        #                            np.array([straightAEnd[0], straightAEnd[2]]),
-        #                            np.array([straightBStart[0], straightBStart[2]]),
-        #                            np.array([straightBEnd[0], straightBEnd[2]]))
-        # M = np.array([M[0], 0, M[1]])
-        #
-        # length = math.pi * np.linalg.norm(straightBStart - straightAEnd) * 0.5
-        # radius1 = np.linalg.norm(M - straightAEnd)
-        # radius2 = np.linalg.norm(M - straightBStart)
-        # print("\n\nRadius1: " + str(radius1))
-        # print("Radius2: " + str(radius2))
-        # theta = length / radius1
-        # tp.append(TrackParts.Arc(straightAEnd, straightBStart, theta, length))
-
-        # nextindex = (i + 1) % len(controlPoints)
-        # endturnindex = (i + 2) % len(controlPoints)
-        # start = np.array(controlPoints[i])
-        # end = np.array(controlPoints[nextindex])
-        #
-        # straightStart = VectorLerp(start, end, 0.1)
-        # straightEnd = VectorLerp(end, start, 0.1)
-        #
-        # tp.append(TrackParts.Straight(straightStart, straightEnd))
-        #
-        # turnEnd = controlPoints[endturnindex]
-        # newTurnEnd = VectorLerp(end, turnEnd, 0.1)
-        # length = math.pi * np.linalg.norm(newTurnEnd - straightEnd) * 0.5
-        # curv = 1 / (np.linalg.norm(turnEnd - start) / 2)
-        # tp.append(TrackParts.Arc(straightEnd, newTurnEnd, curv, length))
+        turnEnd = controlPoints[endturnindex]
+        newTurnEnd = VectorLerp(end, turnEnd, 0.1)
+        length = math.pi * np.linalg.norm(newTurnEnd - straightEnd) * 0.5
+        curv = 1 / (np.linalg.norm(turnEnd - start) / 2)
+        tp.append(TrackParts.Arc(straightEnd, newTurnEnd, curv, length))
 
     return tp
-
-
-def GetTrackParts2(controlPoints):
-    tp = []
-    lastEndPoint = np.array(controlPoints[0])
-
-    for i in range(4):
-        aStartIndex = i
-        aEndIndex = (i + 1) % len(controlPoints)
-        bStartIndex = (i + 2) % len(controlPoints)
-        bEndIndex = (i + 3) % len(controlPoints)
-
-        straightAStart = np.array(controlPoints[aStartIndex])
-        straightAEnd = np.array(controlPoints[aEndIndex])
-        straightBStart = np.array(controlPoints[bStartIndex])
-        straightBEnd = np.array(controlPoints[bEndIndex])
-
-        lenA = np.linalg.norm(straightAEnd - straightAStart)
-        lenB = np.linalg.norm(straightBEnd - straightBStart)
-
-        radius = lenA * 0.4
-        if lenB < lenA:
-            radius = lenB * 0.4
-
-        ArcMiddle, ArcStart, ArcEnd, curvature, length = MathUtils.GetArcMiddle(straightAStart, straightAEnd, straightBStart, straightBEnd, radius, 1)
-
-        ArcStart3D = np.array([ArcStart[0], 0, ArcStart[1]])
-        ArcEnd3D = np.array([ArcEnd[0], 0, ArcEnd[1]])
-
-        tp.append(TrackParts.Straight(lastEndPoint, ArcStart3D))
-        tp.append(TrackParts.Arc(ArcStart3D, ArcEnd3D, curvature, length, ArcEnd))
-        lastEndPoint = ArcEnd3D
-
-    return tp
-
-
-def VectorLerp(A, B, amount: float):
-    V = B - A
-
-    normal = V * (np.array([1, 1, 1]) / np.linalg.norm(V))
-    distance = np.linalg.norm(B - A)
-    result = A + normal * (distance * amount)
-    return result
-
-
-def ExportTrack(controlPoints):
-    """
-    Generate a real Track based on as list of controlpoints
-    :param controlPoints: List of control points defining the corner points of the track
-    :return: None
-    """
-    s = 0
-    for i in range(len(controlPoints)):
-        if i < len(controlPoints) - 1:
-            c = controlPoints[i]
-            hdg = math.atan2(controlPoints[i + 1][2] - controlPoints[i][2],
-                             controlPoints[i + 1][0] - controlPoints[i][0])
-            length = math.sqrt(math.pow(controlPoints[i + 1][2] - controlPoints[i][2], 2) + math.pow(
-                controlPoints[i + 1][0] - controlPoints[i][0], 2))
-            OpenDRIVE_API.generate_line(s, controlPoints[i][0], controlPoints[i][2], hdg, length)
-            s += length
-        else:
-            # letztes Trackstück
-            c = controlPoints[i]
-            hdg = math.atan2(controlPoints[0][2] - controlPoints[i][2], controlPoints[0][0] - controlPoints[i][0])
-            length = math.sqrt(math.pow(controlPoints[0][2] - controlPoints[i][2], 2) + math.pow(
-                controlPoints[0][0] - controlPoints[i][0], 2))
-            OpenDRIVE_API.generate_line(s, controlPoints[i][0], controlPoints[i][2], hdg, length)
-
-    OpenDRIVE_API.generate('E:\\Studium\\dev\\CARLA_0.9.10\\PythonAPI\\util\\opendrive\\rundkurs.xodr')
 
 
 def ExportTrack2(trackParts):
@@ -246,27 +92,6 @@ def ExportTrack2(trackParts):
         s += t.Length
 
     OpenDRIVE_API.generate('E:\\Studium\\dev\\CARLA_0.9.10\\PythonAPI\\util\\opendrive\\rundkurs.xodr')
-
-
-def PlotTrack(controlPoints):
-    """
-    Einen Track mithilfe von Matplotlib plotten
-    :param controlPoints: Liste aus 3er-Tupeln: List of control points defining the corner points of the track
-    :return: None
-    """
-    xv = []
-    yv = []
-    for c in controlPoints:
-        xv.append(c[0])
-        yv.append(c[2])
-
-    xv.append(controlPoints[0][0])
-    yv.append(controlPoints[0][2])
-
-    # Mittelpunkt bei 0, 0
-    plt.scatter(0, 0, c="green")
-    plt.plot(xv, yv)
-    plt.show()
 
 
 def PlotTrack2(trackParts):
@@ -292,21 +117,10 @@ def PlotTrack2(trackParts):
     plt.show()
 
 
-def PlotTrack3(trackParts):
-    # Mittelpunkt bei 0, 0
-    fig, ax = plt.subplots()
+def VectorLerp(A, B, amount: float):
+    V = B - A
 
-    ax.scatter(0, 0, c="green")
-
-    xvStraight = []
-    yvStraight = []
-    for c in trackParts:
-        if type(c) is TrackParts.Straight:
-            xvStraight.append(c.Start[0])
-            yvStraight.append(c.Start[2])
-        else:
-            ax.scatter(float(c.MiddlePoint[0]), float(c.MiddlePoint[1]))
-
-    ax.plot(xvStraight, yvStraight, 'b')
-
-    plt.show()
+    normal = V * (np.array([1, 1, 1]) / np.linalg.norm(V))
+    distance = np.linalg.norm(B - A)
+    result = A + normal * (distance * amount)
+    return result
